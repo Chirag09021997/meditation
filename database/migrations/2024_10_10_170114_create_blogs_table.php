846edce18b6d00bfa4b6c8661a1d8b870d67bfb5
Chirag09021997 <chirag.somecodex@gmail.com>
2024-10-27 15:51:29 +0530

    created_by nullable set

--- database/migrations/2024_10_10_170114_create_blogs_table.php
+++ database/migrations/2024_10_10_170114_create_blogs_table.php
@@ -19,8 +19,8 @@ return new class extends Migration
             $table->string('thumb_image')->nullable();
             $table->integer('total_view')->default(0);
             $table->enum('status', ['Active', 'Inactive'])->default('Active');
-            $table->unsignedBigInteger('created_by')->default(1);
-            $table->unsignedBigInteger('updated_by')->default(1);
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->unsignedBigInteger('updated_by')->nullable();
             $table->timestamps();
             $table->softDeletes();
             $table->foreign('created_by')->references('id')->on('users')->cascadeOnDelete();
