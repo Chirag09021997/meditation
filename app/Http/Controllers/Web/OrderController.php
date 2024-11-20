bf613bee46005fa2e2898a7107dbaba6d579fd20
Chirag09021997 <chirag.somecodex@gmail.com>
2024-11-21 00:48:22 +0530

    order edit changes

--- app/Http/Controllers/Web/OrderController.php
+++ app/Http/Controllers/Web/OrderController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
 use App\Models\Order;
+use App\Models\OrderAddress;
+use App\Models\OrderItems;
 use Illuminate\Http\Request;
 use Yajra\DataTables\Facades\DataTables;
 
@@ -31,7 +33,7 @@ class OrderController extends Controller
      */
     public function edit(string $id)
     {
-        $order = Order::findOrFail($id);
+        $order = Order::with('customer', 'orderAddress', 'orderItem.store')->findOrFail($id);
         return view('order.edit', compact('order'));
     }
 
@@ -41,7 +43,74 @@ class OrderController extends Controller
     public function update(Request $request, string $id)
     {
         $order = Order::findOrFail($id);
-        dd($request->all());
+        $validated = $request->validate([
+            // Billing Information
+            'b_fname' => 'required|string|max:255',
+            'b_lname' => 'required|string|max:255',
+            'b_email' => 'required|email|max:255',
+            'b_phone' => 'required|string|max:15',
+            'b_address' => 'required|string|max:255',
+            'b_address2' => 'nullable|string|max:255',
+            'b_city' => 'required|string|max:255',
+            'b_state' => 'required|string|max:255',
+            'b_country' => 'required|string|max:255',
+            'b_zipcode' => 'required|string|max:255',
+
+            // Shipping Information
+            's_fname' => 'nullable|string|max:255',
+            's_lname' => 'nullable|string|max:255',
+            's_address' => 'nullable|string|max:255',
+            's_address2' => 'nullable|string|max:255',
+            's_city' => 'nullable|string|max:255',
+            's_state' => 'nullable|string|max:255',
+            's_country' => 'nullable|string|max:255',
+            's_zipcode' => 'nullable|string|max:255',
+
+            // Order Information
+            'store_id' => 'required|array|min:1',
+            'store_id.*' => 'exists:stores,id',
+            'price' => 'required|array|min:1',
+            'price.*' => 'numeric|min:0',
+            'discount' => 'required|array|min:1',
+            'discount.*' => 'numeric|min:0',
+            'quantity' => 'required|array|min:1',
+            'quantity.*' => 'integer|min:1'
+        ]);
+
+        // Update order address
+        OrderAddress::where('order_id', $id)->update([
+            'b_fname' => $validated['b_fname'],
+            'b_lname' => $validated['b_lname'],
+            'b_email' => $validated['b_email'],
+            'b_phone' => $validated['b_phone'],
+            'b_address' => $validated['b_address'],
+            'b_address2' => $validated['b_address2'],
+            'b_city' => $validated['b_city'],
+            'b_state' => $validated['b_state'],
+            'b_country' => $validated['b_country'],
+            'b_zipcode' => $validated['b_zipcode'],
+            's_fname' => $validated['s_fname'],
+            's_lname' => $validated['s_lname'],
+            's_address' => $validated['s_address'],
+            's_address2' => $validated['s_address2'],
+            's_city' => $validated['s_city'],
+            's_state' => $validated['s_state'],
+            's_country' => $validated['s_country'],
+            's_zipcode' => $validated['s_zipcode']
+        ]);
+
+        // Update or create order items
+        foreach ($validated['store_id'] as $index => $storeId) {
+            OrderItems::updateOrCreate(
+                ['order_id' => $id, 'store_id' => $storeId],
+                [
+                    'quantity' => $validated['quantity'][$index],
+                    'price' => $validated['price'][$index],
+                    'discount' => $validated['discount'][$index],
+                ]
+            );
+        }
+        return redirect()->route('order.index')->with('success', 'Order Updated SuccessFully.');
     }
 
     /**
@@ -66,7 +135,17 @@ class OrderController extends Controller
             ->addColumn('customer_name', function ($data) {
                 return $data->customer ? $data->customer->name : '';
             })
-            ->rawColumns(['action', 'customer_name'])
+            ->editColumn('status', function ($data) {
+                $statusOptions = ['Pending', 'Shipping', 'Complete'];
+                $statusDropdown = '<select class="status-change rounded-sm" data-order-id="' . $data->id . '">';
+                foreach ($statusOptions as  $statusValue) {
+                    $selected = ($data->status == $statusValue) ? 'selected' : '';
+                    $statusDropdown .= '<option value="' . $statusValue . '" ' . $selected . '>' . $statusValue . '</option>';
+                }
+                $statusDropdown .= '</select>';
+                return $statusDropdown;
+            })
+            ->rawColumns(['action', 'customer_name', 'status'])
             ->addIndexColumn()
             ->toJson();
     }
--- resources/views/order/edit.blade.php
+++ resources/views/order/edit.blade.php
@@ -1,4 +1,7 @@
 <x-app-layout>
+    @php
+        $totalPrice = $totalDiscount = 0;
+    @endphp
     <x-head-lable backhref="{{ route('order.index') }}">
         {{ __('Order Edit') }}
     </x-head-lable>
@@ -6,7 +9,7 @@
     <form method="POST" action="{{ route('order.update', $order->id) }}">
         @csrf
         @method('PUT')
-        <div class="border-4 border-white rounded-lg p-2 sm:p-4 ">
+        <div class="border-4 border-white rounded-lg p-2 sm:p-4 my-2">
             <h1 class="font-bold my-3 text-lg">Billing Information:</h1>
             <div class="grid md:grid-cols-2 gap-4">
                 <!-- b_fname -->
@@ -91,6 +94,145 @@
             </div>
         </div>
 
+        <div class="border-4 border-white rounded-lg p-2 sm:p-4 my-2">
+            <h1 class="font-bold my-3 text-lg">Shipping Information:</h1>
+            <div class="grid md:grid-cols-2 gap-4">
+                <!-- s_fname -->
+                <div class="mt-1">
+                    <x-input-label for="s_fname" :value="__('First Name')" />
+                    <x-text-input type="text" class="block mt-1 w-full" id="s_fname" name="s_fname"
+                        :value="old('s_fname', $order?->orderAddress?->s_fname)" placeholder="Enter first name" required />
+                    <x-input-error :messages="$errors->get('s_fname')" class="mt-2" />
+                </div>
+
+                <!-- s_lname -->
+                <div class="mt-1">
+                    <x-input-label for="s_lname" :value="__('Last Name')" />
+                    <x-text-input type="text" class="block mt-1 w-full" id="s_lname" name="s_lname"
+                        :value="old('s_lname', $order?->orderAddress?->s_lname)" placeholder="Enter last name" />
+                    <x-input-error :messages="$errors->get('s_lname')" class="mt-2" />
+                </div>
+
+                <!-- s_address -->
+                <div class="mt-1">
+                    <x-input-label for="s_address" :value="__('Address')" />
+                    <x-text-input type="text" class="block mt-1 w-full" id="s_address" name="s_address"
+                        :value="old('s_address', $order?->orderAddress?->s_address)" placeholder="Enter address" />
+                    <x-input-error :messages="$errors->get('s_address')" class="mt-2" />
+                </div>
+
+                <!-- s_address2 -->
+                <div class="mt-1">
+                    <x-input-label for="s_address2" :value="__('Address 2')" />
+                    <x-text-input type="text" class="block mt-1 w-full" id="s_address2" name="s_address2"
+                        :value="old('s_address2', $order?->orderAddress?->s_address2)" placeholder="Enter address 2" />
+                    <x-input-error :messages="$errors->get('s_address2')" class="mt-2" />
+                </div>
+
+                <!-- s_city -->
+                <div class="mt-1">
+                    <x-input-label for="s_city" :value="__('City')" />
+                    <x-text-input type="text" class="block mt-1 w-full" id="s_city" name="s_city"
+                        :value="old('s_city', $order?->orderAddress?->s_city)" placeholder="Enter city" />
+                    <x-input-error :messages="$errors->get('s_city')" class="mt-2" />
+                </div>
+
+                <!-- s_state -->
+                <div class="mt-1">
+                    <x-input-label for="s_state" :value="__('State')" />
+                    <x-text-input type="text" class="block mt-1 w-full" id="s_state" name="s_state"
+                        :value="old('s_state', $order?->orderAddress?->s_state)" placeholder="Enter state" />
+                    <x-input-error :messages="$errors->get('s_state')" class="mt-2" />
+                </div>
+
+                <!-- s_country -->
+                <div class="mt-1">
+                    <x-input-label for="s_country" :value="__('Country')" />
+                    <x-text-input type="text" class="block mt-1 w-full" id="s_country" name="s_country"
+                        :value="old('s_country', $order?->orderAddress?->s_country)" placeholder="Enter city" />
+                    <x-input-error :messages="$errors->get('s_country')" class="mt-2" />
+                </div>
+
+                <!-- s_zipcode -->
+                <div class="mt-1">
+                    <x-input-label for="s_zipcode" :value="__('Zipcode')" />
+                    <x-text-input type="text" class="block mt-1 w-full" id="s_zipcode" name="s_zipcode"
+                        :value="old('s_zipcode', $order?->orderAddress?->s_zipcode)" placeholder="Enter state" />
+                    <x-input-error :messages="$errors->get('s_zipcode')" class="mt-2" />
+                </div>
+            </div>
+        </div>
+
+        <div class="border-4 border-white rounded-lg p-4 my-2">
+            <h1 class="font-bold text-lg my-3">Order Information:</h1>
+            <div class="relative overflow-x-auto border-b border-gray-200 dark:border-gray-800">
+                <table class="w-full text-left font-medium text-gray-900 dark:text-white md:table-fixed">
+                    <thead>
+                        <tr>
+                            <th>Img</th>
+                            <th>Price</th>
+                            <th>Discount</th>
+                            <th>Quantity</th>
+                            <th>Final Price</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                </table>
+                @foreach ($order?->orderItem as $item)
+                    @php
+                        $totalPrice += $item->price * $item->quantity;
+                        $totalDiscount += $item->discount * $item->quantity;
+                    @endphp
+                    <div class="grid grid-cols-6 gap-4 py-4 border-b border-gray-300">
+                        <!-- Image and Item Name -->
+                        <div class="flex items-center space-x-4">
+                            <a href="{{ route('store.show', $item->store_id) }}" class="flex-shrink-0">
+                                <img class="h-16 w-16 object-cover" src="{{ $item->store->product_thumb }}"
+                                    alt="Product Image" />
+                            </a>
+                            <div>
+                                <a href="{{ route('store.show', $item->store_id) }}"
+                                    class="text-blue-600 hover:underline">
+                                    <span class="block text-left">{{ $item->store->product_name }}</span>
+                                </a>
+                                <input type="hidden" name="store_id[]" id="store_id"
+                                    value="{{ $item->store_id }}">
+                            </div>
+                        </div>
+                        <!-- Price -->
+                        <div class="text-base font-normal text-gray-900 dark:text-white">
+                            <input type="number" name="price[]" id="price" value="{{ $item->price }}"
+                                onchange="updateFinalPrice(this)"
+                                class="form-input w-full px-3 py-2 border rounded-md" />
+                        </div>
+                        <!-- Discount -->
+                        <div class="text-base font-normal text-gray-900 dark:text-white">
+                            <input type="number" name="discount[]" id="discount" value="{{ $item->discount }}"
+                                onchange="updateFinalPrice(this)"
+                                class="form-input w-full px-3 py-2 border rounded-md" />
+                        </div>
+                        <!-- Quantity -->
+                        <div class="text-base font-normal text-gray-900 dark:text-white">
+                            <input type="number" name="quantity[]" id="quantity" value="{{ $item->quantity }}"
+                                onchange="updateFinalPrice(this)"
+                                class="form-input w-full px-3 py-2 border rounded-md" />
+                        </div>
+                        <!-- Final Price -->
+                        <div class="text-right text-base font-bold text-gray-900 dark:text-white final-price">
+                            ${{ number_format(($item->price - $item->discount) * $item->quantity, 2) }}
+                        </div>
+                        <!-- Delete Button -->
+                        <div class="text-center">
+                            <button type="button" onclick="deleteItem(this)"
+                                class="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500">
+                                Delete
+                            </button>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+
         <div class="flex items-center justify-end mt-4">
             <a class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-200 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 "
                 href="{{ route('order.index') }}">
@@ -101,4 +243,20 @@
                 class="text-white hover:text-blue-900 bg-blue-900 border border-blue-300 focus:outline-none hover:bg-blue-100 focus:ring-4 focus:ring-blue-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 ">{{ __('Update') }}</button>
         </div>
     </form>
+    <script>
+        function updateFinalPrice(element) {
+            let row = element.closest('.grid');
+            let price = parseFloat(row.querySelector('[name="price[]"]').value);
+            let discount = parseFloat(row.querySelector('[name="discount[]"]').value);
+            let quantity = parseInt(row.querySelector('[name="quantity[]"]').value);
+            let finalPrice = (price - discount) * quantity;
+            row.querySelector('.final-price').innerText = "$" + finalPrice.toFixed(2);
+        }
+
+        // Delete the item row
+        function deleteItem(button) {
+            let row = button.closest('.grid');
+            row.remove();
+        }
+    </script>
 </x-app-layout>
