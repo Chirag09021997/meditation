49fd5521e815fb21cf3aabbaac48f6470517c581
SomeCodex <78018898+SomeCodex@users.noreply.github.com>
2025-05-31 14:42:29 +0530

    update policy
    update policy

--- resources/views/frontend/privacy-policy.blade.php
+++ resources/views/frontend/privacy-policy.blade.php
@@ -26,9 +26,55 @@
     <section>
         <div class="container">
             <div class="row align-items-center">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, eveniet. Expedita vero neque ut
-                    voluptatem suscipit soluta odit porro beatae cum sed sequi ex corrupti voluptas tenetur reprehenderit,
-                    aliquid maxime.</p>
+          
+
+<p>TejasAura has established this privacy policy to explain to you how your information is protected, collected and used, which may be updated by TejasAura from time to time. We will provide notice of materially significant changes to this privacy policy by posting notice on this site.</p>
+
+
+<p>&nbsp;</p>
+
+<p><strong>1. Protecting your privacy</strong></p>
+
+<p>Account information is password-protected. Keep your password safe.</p>
+
+<p>TejasAura.com, or people who post on TejasAura.com, may provide links to third party websites, which may have different privacy practices. We are not responsible for, nor have any control over, the privacy policies of those third party websites, and encourage all users to read the privacy policies of each and every website visited.</p>
+
+<p>&nbsp;</p>
+
+<p><strong>2. Data we collect</strong></p>
+
+<p>We sometimes collect your email address, for purposes such as sending self-publishing and confirmation emails, authenticating user accounts, providing subscription email services, registering for forums, etc.</p>
+
+<p>TejasAura does not store credit card information. Credit card transactions are transmitted to a financial gateway, and we endeavor to protect the security of your payment information during transmission by using Secure Sockets Layer (SSL) technology.</p>
+
+<p>Our web logs collect standard web log entries for each page served, including your IP address, page URL, and timestamp. Web logs help us to diagnose problems with our server, to administer the TejasAura.com website, and to otherwise provide our service to you.</p>
+
+
+<p>&nbsp;</p>
+
+<p><strong>3. Data we store</strong></p>
+
+<p>All postings are stored in our database, even after “deletion,” and may be archived elsewhere.</p>
+
+<p>Our web logs and other records are stored indefinitely.</p>
+
+<p>Although we make good faith efforts to store the information in a secure operating environment that is not available to the public, we cannot guarantee complete security.</p>
+
+<p>&nbsp;</p>
+
+<p><strong>4. Circumstances in which TejasAura may release information</strong></p>
+
+<p>TejasAura.com may disclose information about its users if required to do so by law or in the good faith belief that such disclosure is reasonably necessary to respond to subpoenas, court orders, or other legal process.</p>
+
+<p>TejasAura.com may also disclose information about its users to law enforcement officers or others, in the good faith belief that such disclosure is reasonably necessary to: enforce our Terms of Use; respond to claims that any posting or other content violates the rights of third-parties; or protect the rights, property, or personal safety of TejasAura.com, its users or the general public.</p>
+
+<p>&nbsp;</p>
+
+<p><strong>5. International users</strong></p>
+
+<p>By visiting our web site and providing us with data, you acknowledge and agree that due to the international dimension of TejasAura.com we may use the data collected in the course of our relationship for the purposes identified in this policy or in our other communications with you, including the transmission of information outside your resident jurisdiction. In addition, please understand that such data may be stored on servers located in the United States. By providing us with your data, you consent to the transfer of such data.</p>
+
+<p>&nbsp;</p>
             </div>
         </div>
     </section>
--- resources/views/frontend/term-condition.blade.php
+++ resources/views/frontend/term-condition.blade.php
@@ -26,9 +26,204 @@
     <section>
         <div class="container">
             <div class="row align-items-center">
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis doloremque libero facilis minima
-                    aperiam dolore error quia repudiandae deserunt eligendi natus, ipsa sapiente fugit magnam labore. Optio
-                    modi ullam perferendis?</p>
+                <p><strong>This online store is operated by Commercial and Technical Services. (&quot;We&quot; or &quot;Our&quot; or &quot;Us&quot;, wherever applicable) accessible at catsestore.com (&quot;Website&quot;). Please read these conditions (&quot;Terms of Services&quot;) carefully before placing an order for any products. You (&quot;Customer&quot;) are agreeing to be bound by the following terms and conditions. If you do not agree to all the terms and conditions of this agreement, then you may not access the Website or use any services.</strong></p>
+
+<p><strong>You have read and agree to the Privacy Policy and Returns Policy available on our Website.</strong></p>
+
+<p>&nbsp;</p>
+
+<p>You can review the most current version of the Terms of Service at any time on this page. We reserve the right to update, change or replace any part of these Terms of Service by posting updates and/or changes to our website. It is your responsibility to check this page periodically for changes. Your continued use of or access to the website following the posting of any changes constitutes acceptance of those changes</p>
+
+<p>Our store is hosted on www.linker.store. They provide us with the online e-commerce platform that allows us to sell our products and services to you. We reserve the right to terminate your use of the Service or any related website or taking suitable legal action against you for violating this Terms of Service.</p>
+
+<p>&nbsp;</p>
+
+<p><strong>1. Account Terms</strong></p>
+
+<p>1. By agreeing to these Terms of Service, you represent that you are at least the age of 18 years or the age of majority in your state or province or country of residence, or that you are of the age of majority in your state or province or country of residence and you have given us your consent to allow any of your minor dependents to use this site.</p>
+
+<p>2. The website requires you to register as a user by creating an account so that you purchase products listed. You solemnly agree to provide true and accurate information about yourself during the registration process (&quot;My Account&quot;) at the website. If we have reasonable grounds to believe that the information provided by you is untrue or inaccurate, we reserve the right to suspend or terminate your account and refuse you any and all current use or future use of the website (or any portion thereof).</p>
+
+<p>3. We reserve the right to refuse Service, terminate your account, or remove or edit contents at our sole discretion without assigning any reason whatsoever.</p>
+
+<p>4. You also take full responsibility for any and all transactions emanating through your account. As such, you should ensure full confidentiality of your account details, and should you become aware of any unauthorized use of your account, you must inform us immediately of the same. Further, you must always exit at the end of each session so that another user may not use your account.</p>
+
+<p>5. We will not be liable for any loss or damage arising to you from your failure to ensure confidentiality of your account details or use by anyone of your account.</p>
+
+<p>6. However, you may be held responsible and liable should losses or damages occur to Us due to your failure to comply with the &quot;user&quot; obligations as mentioned in this Terms of Service.</p>
+
+<p>7. The website also allows limited access to unregistered users who however have to register once they intend to purchase a product. Use of another registered user&#39;s account information for availing the services provided by the website is prohibited.</p>
+
+<p>8. You agree to ensure that your account information is always up to date.</p>
+
+<p>9. You may not use our products for any illegal or unauthorized purpose nor may you, in the use of the Service, violate any laws in your jurisdiction (including but not limited to copyright laws).</p>
+
+<p>10. A breach or violation of any of the Terms herein will result in the immediate termination of our Service to you, and possible legal action under the prevailing laws.</p>
+
+<p>11. You agree that you are solely responsible for your account and your conduct with regard to the access and usage of the website and related servers and systems including but not limited to any loss or damage which We or our affiliates may suffer as a consequence of your actions which contravene any terms of this agreement or Indian or international laws.</p>
+
+<p>&nbsp;</p>
+
+<p><strong>2. User Obligations</strong></p>
+
+<p>The website operation is governed by the Government of India IT Act of 2000 and other relevant revisions and amendments and other acts and laws that are promulgated by the Indian Government from time to time. As such, anything contained herein is superseded by any such Government laws, if anything contained herein is contrary to the same.</p>
+
+<p>In addition to other prohibitions as set forth elsewhere in this Terms of Service, you are prohibited from using the website or its content:</p>
+
+<p>a. for any unlawful purpose;</p>
+
+<p>b. to solicit others to perform or participate in any unlawful acts;</p>
+
+<p>c. to violate any international, federal, provincial or state regulations, rules, laws, or local ordinances;</p>
+
+<p>d. to infringe upon or violate our intellectual property rights or the intellectual property rights of others including suppliers, Shopify, etc.;</p>
+
+<p>e. to harass, abuse, insult, harm, defame, slander, disparage, intimidate, or discriminate based on gender, sexual orientation, religion, ethnicity, race, age, national origin, or disability;</p>
+
+<p>f. to submit false or misleading information;</p>
+
+<p>g. to upload or transmit worms, viruses or any other type of malicious code that will or may be used in any way that will affect the functionality or operation of the Service or of any related website, other websites, or the Internet;</p>
+
+<p>h. to collect or track the personal information of others;</p>
+
+<p>i. to spam, phish, pharm, pretext, spider, crawl, or scrape;</p>
+
+<p>j. for any obscene or immoral purpose;</p>
+
+<p>k. to interfere with or circumvent the security features of the Service or any related website, other websites, or the Internet.</p>
+
+<p>l. to engage in any activity that will interfere or disrupt user access or user transaction on the website.</p>
+
+<p>m. to attempt to gain unauthorized access to any portion or feature or data available on the website and related servers or any other systems or networks connected to the website or any of the services offered on or through the website by hacking or using any other illegal or illegitimate means.</p>
+
+<p>n. to access or attempt to access the website through any other means and technologies other than what has been provided by Us. You also agree not to copy, reproduce, deface or alter any content available on the website.</p>
+
+<p>o. to reproduce, duplicate, copy, sell, resell or exploit any portion of the Service, use of the Service, or access to the Service or any contact on the website through which the service is provided, without express written permission by us.</p>
+
+<p>The headings used in this agreement are included for convenience only and will not limit or otherwise affect these terms.</p>
+
+<p>&nbsp;</p>
+
+<p><strong>3. Product Information &amp; Orders</strong></p>
+
+<p>1. Your order is an offer to us to buy the product(s) in your order. When you place an order to purchase a product from us, you will receive an e-mail confirming receipt of your order and containing the details of your order (the &quot;Order Confirmation E-mail&quot;). The Order Confirmation E-mail is acknowledgement that we have received your order, and does not confirm acceptance of your offer to buy the product(s) ordered.</p>
+
+<p>2. You confirm that the product(s) ordered by you are purchased for your internal / personal purpose and not for re-sale. You authorize us to declare and provide declaration to any governmental authority on your behalf stating the aforesaid purpose of the products ordered by you on the website.</p>
+
+<p>3. We have made every effort to display as accurately as possible the colors and images of our products that appear at the website. We however, cannot guarantee that your computer monitor&#39;s display of any color will be accurate.</p>
+
+<p>4. Most products displayed are limited in quantities and as such, we cannot guarantee that we can provide you with an identical product should the one displayed on the website be sold out.</p>
+
+<p>5. We reserve the right, but are not obligated, to limit the sales of our products or Service to any person, geographic region or jurisdiction. We may exercise this right on a case-by-case basis.</p>
+
+<p>6. We reserve the right to limit the quantities of any products or services that we offer.</p>
+
+<p>7. We may, at our sole discretion, limit or cancel quantities purchased per person, per household or per order. These restrictions may include orders placed by or under the same customer account, the same credit card, and/or orders that use the same billing and/or shipping address. In the event that we make a change to or cancel an order, we may attempt to notify you by contacting the e-mail and/or billing address/phone number provided at the time the order was made. We reserve the right to limit or prohibit orders that, in our sole judgment, appear to be placed by dealers, resellers or distributors.</p>
+
+<p>8. We reserve the right to discontinue any product at any time. Any offer for any product or service made on this site is void where prohibited.</p>
+
+<p>9. We reserve the right to change the pricing of any product at any time without assigning any reason and without any prior notice</p>
+
+<p>10. We reserve the right to refuse or cancel any order without assigning any reason. However, some of the reasons why we could refuse or cancel orders include but is not limited to:</p>
+
+<p>a. Shortage/non availability of the product.</p>
+
+<p>b. Inaccuracies or errors in product pricing information displayed at the site.</p>
+
+<p>c. Product defects observed prior to dispatch.</p>
+
+<p>d. Inability of supplier to supply.</p>
+
+<p>e. Any quality related issues.</p>
+
+<p>f. If we feel that the customer is committing any sort of fraud.</p>
+
+<p>g. If your order was cancelled by us after your credit card has been charged, the applicable amount shall be credited back by us via Net Banking, as a reversal on your credit card account, or by any means that we deem to be convenient.</p>
+
+<p>h. All types and descriptions of products or product pricing are subject to change at any time without notice at our sole discretion.</p>
+
+<p>i. All products sold at the website are subject to return, exchange and cancellation according to our Return, Exchange and Cancellation Policy available at the website and this is read an accepted by you.</p>
+
+<p>j. We do not warrant that the quality of any products, services, information, or other material purchased or obtained by you from our website will meet your expectations, or that any errors in the Service will be corrected.</p>
+
+<p>&nbsp;</p>
+
+<p><strong>4. Taxes</strong></p>
+
+<p>You shall be responsible for payment of all fees/costs/charges associated with the purchase of products from us and you agree to bear any and all applicable taxes/VAT, cesses etc. levied thereon.</p>
+
+<p>&nbsp;</p>
+
+<p><strong>5. Communication</strong></p>
+
+<p>When you send e-mails to us, you are communicating with us electronically. You will be required to provide a valid phone number while placing an order with us. We may communicate with you by e-mail, SMS, phone call or by posting notices on the website or by any other mode of communication. For contractual purposes, you consent to receive communications including WhatsApp, SMS, e-mails or phone calls from us with respect to your order.</p>
+
+<p>&nbsp;</p>
+
+<p><strong>6. Copyrights and Trademarks</strong></p>
+
+<p>1. The site contains trademarks, marks, and copyrighted proprietary information/material including text, photographs, logos, software, videos, sound, graphics, music and company information owned by us, our suppliers, our partners and/or other third parties.</p>
+
+<p>2. You are prohibited from using any marks and any information/material provided on the website for any purpose whatsoever without written permission from us, suppliers, partners or third parties as applicable who may own the marks or proprietary information.</p>
+
+<p>3. You are prohibited from copying, modifying, distributing, publishing, selling, licensing, redistribution, retransmission, creating derivative work or using any content available at the site for private, commercial purposes or public purposes. You must implement the relevant Indian and international laws on copyright and trademarks as applicable.</p>
+
+<p>&nbsp;</p>
+
+<p><strong>7. Communication</strong></p>
+
+<p>When you send e-mails to us, you are communicating with us electronically. You will be required to provide a valid phone number while placing an order with us. We may communicate with you by e-mail, SMS, phone call or by posting notices on the website or by any other mode of communication. For contractual purposes, you consent to receive communications including WhatsApp, SMS, e-mails or phone calls from us with respect to your order.</p>
+
+<p>&nbsp;</p>
+
+<p><strong>8. Indemnification &amp; Limitation of Liability</strong></p>
+
+<p>1. In no case shall we, our directors, officers, employees, affiliates, agents, partners, contractors, interns, suppliers, service providers or licensors be liable for any injury, loss, claim, or any direct, indirect, incidental, punitive, special, or consequential damages of any kind, including, without limitation lost profits, lost revenue, lost savings, loss of data, replacement costs, or any similar damages, whether based in contract, tort (including negligence), strict liability or otherwise, arising from your use of any of the service or any products procured using the service, or for any other claim related in any way to your use of the service or any product, including, but not limited to, any errors or omissions in any content, or any loss or damage of any kind incurred as a result of the use of the service or any content (or product) posted, transmitted, or otherwise made available via the service, even if advised of their possibility.</p>
+
+<p>2. Notwithstanding anything to the contrary, our entire liability to you under this Terms of Service shall be the refund of the moneys charged from you under which the unlikely liability arises.</p>
+
+<p>3. You agree to indemnify, defend and hold harmless us and our parent, subsidiaries, affiliates, partners, officers, directors, agents, contractors, licensors, service providers, subcontractors, suppliers, interns and employees, harmless from any claim or demand, including reasonable attorneys&#39; fees, made by any third-party due to or arising out of your breach of these Terms of Service or the documents they incorporate by reference, or your violation of any law or the rights of a third-party.</p>
+
+<p>4. Neither we nor any third parties provide any warranty or guarantee as to the accuracy, timeliness, performance, completeness or suitability of the information and materials found or offered on this website for any particular purpose. You acknowledge that such information and materials may contain inaccuracies or errors and we expressly exclude liability for any such inaccuracies or errors to the fullest extent permitted by law.</p>
+
+<p>&nbsp;</p>
+
+<p><strong>9. Termination</strong></p>
+
+<p>1. The obligations and liabilities of the parties incurred prior to the termination date shall survive the termination of this agreement for all purposes.</p>
+
+<p>2. These Terms of Service are effective unless and until terminated by either you or us. You may terminate these Terms of Service at any time by notifying us that you no longer wish to use our Services, or when you cease using our site.</p>
+
+<p>3. If in our sole judgment you fail, or we suspect that you have failed, to comply with any term or provision of these Terms of Service, we also may terminate this agreement at any time without notice and you will remain liable for all amounts due up to and including the date of termination; and/or accordingly may deny you access to our Services (or any part thereof).</p>
+
+<p>&nbsp;</p>
+
+<p><strong>10. Entire Agreement</strong></p>
+
+<p>1. The failure of us to exercise or enforce any right or provision of these Terms of Service shall not constitute a waiver of such right or provision</p>
+
+<p>2. These Terms of Service and any policies or operating rules posted by us on this site or in respect to The Service constitutes the entire agreement and understanding between you and us and govern your use of the Service, superseding any prior or contemporaneous agreements, communications and proposals, whether oral or written, between you and us (including, but not limited to, any prior versions of the Terms of Service).</p>
+
+<p>3. Any ambiguities in the interpretation of these Terms of Service shall not be construed against the drafting party.</p>
+
+<p>&nbsp;</p>
+
+<p><strong>11. Governing Law</strong></p>
+
+<p>These Terms of Service and other policies and any other separate agreements whereby we provide you shall be governed by and construed in accordance with the laws of India with exclusive jurisdiction to the courts of Surat, Gujarat, India.</p>
+
+<p>&nbsp;</p>
+
+<p><strong>12. Waiver</strong></p>
+
+<p>If you breach these conditions and we take no action, we will still be entitled to use our rights and remedies in any other situation where you breach these conditions.</p>
+
+<p>&nbsp;</p>
+
+<p><strong>13. Miscellaneous</strong></p>
+
+<p>We shall not entertain any complaints beyond 10 days of the order being delivered, including the delivery date.</p>
+
             </div>
         </div>
     </section>
